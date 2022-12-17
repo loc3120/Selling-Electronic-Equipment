e9ddbdadf4b4b30352584bb25d4d87c2bb2dde65
BubbleNguyen <loc3120@gmail.com>
2022-12-18 01:16:35 +0700

    First commit

---
+++ src/main/java/com/example/sellingelectronicequipment/SellingElectronicEquipmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.sellingelectronicequipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellingElectronicEquipmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SellingElectronicEquipmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sellingelectronicequipment/SellingElectronicEquipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sellingelectronicequipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellingElectronicEquipmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
